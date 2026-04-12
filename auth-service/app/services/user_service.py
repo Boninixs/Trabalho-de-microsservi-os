7e39adbf90e26d96fceaa1ced39fb4cd65c6aa39
Boninixs <119002985+Boninixs@users.noreply.github.com>
2026-04-12 13:39:27 -0300

    jwt + login

--- auth-service/app/services/user_service.py
+++ auth-service/app/services/user_service.py
@@ -1,6 +1,7 @@
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from app.repositories.user_repository import get_user_by_email, create_user
+from app.core.security import verify_password, create_access_token
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -14,3 +15,16 @@ def create_user_service(db: Session, email: str, password: str):
     hashed_password = pwd_context.hash(password)
 
     return create_user(db, email, hashed_password)
+
+def login_user_service(db, email: str, password: str):
+    user = get_user_by_email(db, email)
+
+    if not user:
+        raise Exception("Usuário não encontrado")
+
+    if not verify_password(password, user.password):
+        raise Exception("Senha inválida")
+
+    token = create_access_token({"sub": user.email})
+
+    return token
